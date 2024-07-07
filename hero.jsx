9cd95b4b8246b02b30bb5cf9b63cd3d499a04447
VJ13SS <135973734+VJ13SS@users.noreply.github.com>
2024-07-07 14:26:10 +0530

    Create hero.jsx

---
+++ hero.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Hero() {
+   return(
+     <div className = 'hero'>
+       <h1>We Ensure Better Education For a Better World</h1>
+       <p>Deemed for Aspiring brilliant minds around the world</p>
+         <button className = 'hero-button'>Explore more<img src = "src/Components/Images/dark-arrow.png" /></button>
+       
+     </div>
+   )
+}
