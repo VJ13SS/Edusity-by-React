0e4aa99e7e892103c79ca7b84f081fd7ec8ec07c
VJ13SS <135973734+VJ13SS@users.noreply.github.com>
2024-07-07 14:25:38 +0530

    Create Footer.jsx

---
+++ Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Footer() {
+   return(
+     <footer>
+       <p>©2024 All rights Reserved </p>
+       <p>Terms of Service </p>
+       <p>Privacy Policy</p>
+     </footer>
+   )
+}
