6eb1b9ff71fe2470eb952aa0124cc0d0ff011da8
VJ13SS <135973734+VJ13SS@users.noreply.github.com>
2024-07-07 14:21:42 +0530

    Create About.jsx

---
+++ About.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default function About() {
+   return(
+     <div className = "about" id = "About">
+       <div className = "about-video">
+       <img src = "src/Components/Images/about.png"/></div>
+       <div className = "about-content">
+         <h4>About University</h4>
+         <h2>Nurturing Tomorrow's Leaders Today</h2>
+         <p>Edusity University is a premier institution dedicated to excellence in education and research</p>
+         <p>Edusity provides wide ranges of UnderGraduate, Graduate,PHD programms which helps scholars to equip the excellence and expertise in their choses field</p>
+         <p>At Edusity, we nurture the leaders of tomorrow, fostering a culture of academic rigor, critical thinking, and lifelong learning.</p>
+       </div>
+     </div>
+   )
+}
